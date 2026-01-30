8d12b90f797365184390c56c08fad0c9e1d75045
Lingfeng <lingfengsun@berkeley.edu>
2026-01-30 13:51:02 -0500

    Initial commit: NovaPlan website template

---
+++ assets/js/task-switcher.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const taskButtonsContainer = document.getElementById('task-buttons');
+  const taskViewer = document.getElementById('task-viewer');
+  const initialObservationImg = document.getElementById('initial-observation');
+  const generatedVideo = document.getElementById('generated-video');
+  const executionVideo = document.getElementById('execution-video');
+
+//   const buildSrc = (task) => `assets/viser-client/?playbackPath=assets/videos/real-world/${task}.viser&initialCameraPosition=0,0,0&initialCameraLookAt=0.0,0.0,1.0&initialCameraUp=0.0,-1.0,0.0`;
+  const buildSrc = (task) => `assets/viser-client/?playbackPath=../example_recording.viser&initialCameraPosition=0,0,0&initialCameraLookAt=0.0,0.0,1.0&initialCameraUp=0.0,-1.0,0.0`;
+  
+  const updateMediaSources = (task) => {
+    // a dictionary that maps tasks to their media files
+    const media_path = `assets/example_task_assets`
+    initialObservationImg.src = `${media_path}/start.png`;
+    generatedVideo.src = `${media_path}/video.mp4`;
+    executionVideo.src = `${media_path}/execution.mp4`;
+    taskViewer.src = buildSrc(task);
+  }
+
+  taskButtonsContainer.addEventListener('click', function(e) {
+    if (e.target.classList.contains('task-button')) {
+      // Remove active class from all buttons
+      taskButtonsContainer.querySelectorAll('.task-button').forEach(button => {
+        button.classList.remove('active');
+      });
+
+      // Add active class to the clicked button
+      e.target.classList.add('active');
+
+      // Update the iframe source
+      const selectedTask = e.target.dataset.value;
+      updateMediaSources(selectedTask);
+    }
+  });
+
+  // Set the initial view
+  const initialButton = taskButtonsContainer.querySelector('.task-button.active');
+  if (initialButton) {
+    const initialTask = initialButton.dataset.value;
+    updateMediaSources(initialTask);
+  }
+});
